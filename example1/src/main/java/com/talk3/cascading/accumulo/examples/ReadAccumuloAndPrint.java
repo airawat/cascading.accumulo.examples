aaff888ab06a51b1d535141cf5230fdb8505d4cc
aakT3 <anagha.a.khanolkar@gmail.com>
2014-02-21 15:38:27 -0600

    Updated

--- example1/src/main/java/com/talk3/cascading/accumulo/examples/ReadAccumuloAndPrint.java
+++ example1/src/main/java/com/talk3/cascading/accumulo/examples/ReadAccumuloAndPrint.java
@@ -1,17 +1,33 @@
 package com.talk3.cascading.accumulo.examples;
 
-import cascading.accumulo.*;
 import java.util.Properties;
 import org.apache.hadoop.mapred.JobConf;
 import cascading.property.AppProps;
 import cascading.flow.hadoop.HadoopFlowProcess;
 import cascading.tuple.TupleEntryIterator;
-
-
+import com.talk3.cascading.accumulo.AccumuloScheme;
+import com.talk3.cascading.accumulo.AccumuloTap;
         
 public class ReadAccumuloAndPrint {
 
 	public static void main(String[] args) throws Exception {
+            if(args.length == 0)
+            {
+                throw new Exception("Insufficient paramaters to run the program!");
+            }
+            
+            
+                // {{
+		// ARGUMENTS
+                String accumuloConnectionString = args[0].toString();
+                String accumuloQueryCriteria="";
+                
+                if(args.length ==2)
+                    accumuloQueryCriteria = args[1].toString();
+                
+                // }}
+            
+            
 		// {{
 		// JOB 
 		JobConf jobConf = new JobConf();
@@ -20,13 +36,6 @@ public class ReadAccumuloAndPrint {
 		Properties properties = AppProps.appProps()
 				.setName("ReadAccumuloAndPrint").setVersion("1.0.0")
 				.buildProperties(jobConf);
-		// }}
-
-		// {{
-		// ARGUMENTS
-		String accumuloConnectionString = args[0].toString();
-		String accumuloQueryCriteria = args[1].toString();
-
 		// }}
 
 		// {{
